47f610aa25f0a87650fe0bb8152651b55eedfae2
abourne1 <abourne1@uchicago.edu>
2015-06-28 00:32:35 -0700

    pushing to heroku

--- phone_interview/app/__init__.py
+++ phone_interview/app/__init__.py
@@ -1,10 +1,45 @@
+import datetime
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
+from sqlalchemy.dialects.postgresql import JSON
 
 app = Flask(__name__)
 app.config.from_pyfile("../config.py")
 db = SQLAlchemy(app)
 
-from app import views, models
+from app import views
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(64))
+    topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'))
+    text = db.Column(JSON)
+    hint = db.Column(JSON)
+    difficulty = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+
+    def __init__(self, text, hint, topic_id, author, difficulty):
+        self.author = author
+        self.topic_id = topic_id
+        self.difficulty = difficulty
+        self.text = text
+        self.hint = hint
+        self.timestamp = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+class Topic(db.Model):
+    __tablename__ = 'topics'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
 
